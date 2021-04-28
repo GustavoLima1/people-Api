b397af90dcc44309152aa7a55b2d297a3e578ce6
Gustavo Lima <gustavo1.l@hotmail.com>
2021-04-28 16:04:13 -0300

    Created MessageResponseDTO and PersonController with end point post

---
+++ src/main/java/one/digital/personapi/controller/PersonController.java
@@ -0,0 +1,31 @@
+package one.digital.personapi.controller;
+
+import one.digital.personapi.dto.MessageResponseDTO;
+import one.digital.personapi.entity.Person;
+import one.digital.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/people")
+public class PersonController {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController( PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID" + savedPerson.getId())
+                .build();
+    }
+}
